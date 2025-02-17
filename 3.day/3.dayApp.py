9ea5a25d310aacca344c7d4492c4f099a403ebc9
DESKTOP-E23IDKP\Abdullah Morsy <abdallamorsii@gmail.com>
2025-02-17 14:18:18 +0300

    3.day projects done

---
+++ 3.day/3.dayApp.py
@@ -0,0 +1,13 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+country = input("What country are you from?: ")
+dreamCollege = input("What college do you dream of going to?: ")
+GPA = input("What is your GPA?: ")
+SAT = input("What is your SAT score?: ")
+
+print("Hello " + name + "!")
+print("You are " + age + " years old and you want to go to " + dreamCollege + ".I hope you get in! It's amazing to dreaming big!")
+print("You are from " + country + " and you have a GPA of " + GPA + ". You also have an SAT score of " + SAT + ". You're doing great!")
+print("you can do it!")
+
+print("You can follow my progress at replit.com/@AbdullahMorsy")
