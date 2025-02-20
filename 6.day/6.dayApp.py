7befc54a0a9ebc6bf96dbeff97fcf415976c6145
DESKTOP-E23IDKP\Abdullah Morsy <abdallamorsii@gmail.com>
2025-02-20 21:03:03 +0300

    6.day project is done

---
+++ 6.day/6.dayApp.py
@@ -0,0 +1,18 @@
+print("My login System")
+print("+++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+if username == "Abdullah" and password == "Abc123":
+  print("Welcome Abdullah, you are looking nice today!")
+elif username == "Eslem" and password == "sena123":
+  print("Hi Eslem, you looking good today!")
+elif username == "Mustafa" and password == "19032004":
+  print("Hi Mustafa, are you ready to begain!")
+elif username == "Meryem" and password == "Akkilic" :
+  print("Hi Meryem, you are looking good today!")
+elif username == "Murat" and password == "Murat123":
+  print("Hi Murat, how it's going!")
+elif username == "Nursima" and password == "buyuk":
+  print("Hi Nursima, you are looking good today!")
+else :
+  print("Go away!")
