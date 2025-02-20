0a2e29bdca992896b4e0bc4ae20997a3c610a88a
DESKTOP-E23IDKP\Abdullah Morsy <abdallamorsii@gmail.com>
2025-02-20 19:02:13 +0300

    5. day project is done

---
+++ 5.day/5.dayApp.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("i will ask you some questions. and you have to answer them by yes or no")
+
+playingGames = input("do you like to play games?")
+cookFood = input("do you like to cook food?")
+Coding = input("do you like to code?")
+if playingGames == "yes":
+  print("you are a gamer")
+if cookFood == "yes":
+  print("you are a chef")
+if Coding == "yes":
+  print("you are a Programmer")
+else:
+  print("you are a lazy person")
+  
