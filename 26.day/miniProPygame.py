ad50294943661c9e14740913c2e6fd94de79c55e
DESKTOP-E23IDKP\Abdullah Morsy <abdallamorsii@gmail.com>
2025-03-02 18:17:49 +0300

    26.day 3.project is done

---
+++ 26.day/miniProPygame.py
@@ -0,0 +1,47 @@
+import pygame
+
+pygame.init()
+
+# Pencere boyutları
+WIDTH, HEIGHT = 1000, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pygame Top Hareketi")
+
+pygame.mixer.init()
+pygame.mixer.music.load('audio.wav')
+pygame.mixer.music.play()
+
+# Renkler
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Topun başlangıç konumu
+x, y = 400, 300
+radius = 20
+speed = 5
+
+running = True
+while running:
+    pygame.time.delay(30)  # FPS ayarı
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+
+    keys = pygame.key.get_pressed()  # Tuşları oku
+    if keys[pygame.K_LEFT]:
+        x -= speed
+    if keys[pygame.K_RIGHT]:
+        x += speed
+    if keys[pygame.K_UP]:
+        y -= speed
+    if keys[pygame.K_DOWN]:
+        y += speed
+
+    screen.fill(WHITE)  # Ekranı temizle
+    pygame.draw.circle(screen, RED, (x, y), radius)  # Topu çiz
+    pygame.display.update()  # Ekranı güncelle
+
+pygame.quit()
